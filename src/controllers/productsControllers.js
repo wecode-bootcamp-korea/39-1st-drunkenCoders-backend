6078b3ff96b3291e8df437e17a430c4797804a37
MatheGoD <mathe1303@gmail.com';:Wq:wq>
2022-11-21 18:53:05 +0900

    try & catch 정리

--- src/controllers/productsControllers.js
+++ src/controllers/productsControllers.js
@@ -1,14 +1,12 @@
 const productsService = require("../services/productsService");
 
 const productsAll = async (req, res) => {
-  try {
     const { cate_id , sweetness , sourness , carbon , fruit , flower , grain , priceRange , alchol , sort } = req.query
+
     const products = await productsService.productsAll(
       cate_id , sweetness , sourness , carbon , fruit , flower , grain , priceRange , alchol , sort);
+    
     return res.status(200).json({ data: products });
-  } catch (err) {
-    res.status(err.statusCode || 500).json({ message: err.message });
-  }
 };
 
 module.exports = { productsAll };
