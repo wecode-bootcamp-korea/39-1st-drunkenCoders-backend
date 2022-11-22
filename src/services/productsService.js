9eb04aef6c3d0a5946273061f774bdc5965ed1be
MatheGoD <mathe1303@gmail.com';:Wq:wq>
2022-11-22 14:17:46 +0900

    모듈화 진행 중

--- src/services/productsService.js
+++ src/services/productsService.js
@@ -1,6 +1,7 @@
 const productsDao = require("../models/productsDao");
+const whereList = require("../utils/whereList")
 
-const productsAll = async (
+const getAllProducts = async (
   cate_id , sweetness , sourness , carbon , fruit , flower , grain , priceRange , alchol , sort) => {
     const sortCate = (sort) => {
     const sorting = {
@@ -19,51 +20,12 @@ const productsAll = async (
     }
   };
 
-  const whereList = (
-    cate_id , sweetness , sourness , carbon , fruit , flower , grain , priceRange , alchol ) => {
-    const startLine = "WHERE ";
-    const filter = ["p.id IS NOT NULL"];
-    if (cate_id) {
-      filter.push(`categories_id=${cate_id}`);
-    }
-    if (sweetness) {
-      filter.push(`sweetness_id=${sweetness}`);
-    }
-    if (sourness) {
-      filter.push(`sourness_id=${sourness}`);
-    }
-    if (carbon) {
-      filter.push(`carbon_id=${carbon}`);
-    }
-    if (fruit) {
-      filter.push(`fruit_id=${fruit}`);
-    }
-    if (flower) {
-      filter.push(`flower_id=${flower}`);
-    }
-    if (grain) {
-      filter.push(`grain_id=${grain}`);
-    }
-    if (priceRange) {
-      filter.push(`price BETWEEN ${priceRange[0]} AND ${priceRange[1]}`);
-    }
-    if (alchol) {
-      filter.push(`alchol BETWEEN ${alchol[0]} AND ${alchol[1]}`)
-    }
-    const body = filter.join(" AND ");
-    const combined = startLine + body;
-    return {
-      toSqlString: function () {
-        return combined;
-      },
-    };
-  };
+  const whereCond = whereList.makeWhereList(cate_id , sweetness , sourness , carbon , fruit , flower , grain , priceRange , alchol)
 
-  const products = await productsDao.productsAll(
-    whereList(cate_id , sweetness , sourness , carbon , fruit , flower , grain , priceRange , alchol),
+  const products = await productsDao.getAllProducts(
+    whereCond,
     sortCate(sort)
   );
-
   return products;
 };
 
