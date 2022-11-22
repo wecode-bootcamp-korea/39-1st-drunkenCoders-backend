3a28407b229b1e7b89c81419c11d9ca0bb607709
MatheGoD <mathe1303@gmail.com';:Wq:wq>
2022-11-22 17:35:33 +0900

    제품 상세 리스트

--- src/models/productsDao.js
+++ src/models/productsDao.js
@@ -38,9 +38,15 @@ const getProductDetails = async (productId) => {
   const details = await appDataSource.query(
     `
     SELECT 
-      p.name, 
+      p.id,
+      p.name as product_name, 
+      p.description, 
       p.price, 
       pi.image_url, 
+      p.capacity_ml, 
+      p.alchol, 
+      cate.name, 
+      p.detail_image,
       JSON_ARRAYAGG(
         JSON_OBJECT(
           "tags",t.name
@@ -54,22 +60,17 @@ const getProductDetails = async (productId) => {
       AVG(c.rating)*100/5 
       FROM comments c 
       WHERE c.product_id = p.id 
-      GROUP BY c.product_id) as ratings, 
-    (SELECT 
-      JSON_ARRAYAGG(
-        JSON_OBJECT(
-          "title",c.title,"content",c.content)) 
-      FROM comments c 
-      WHERE c.product_id = p.id 
-      GROUP BY product_id) as posts
-    FROM products p  
-    LEFT JOIN product_images pi ON p.id = pi.product_id  
-    LEFT JOIN tags t ON p.id = t.product_id
-    WHERE p.id = ?  
+      GROUP BY c.product_id) as ratings
+    FROM products p 
+    LEFT JOIN product_images pi ON p.id = pi.product_id
+    LEFT JOIN tags t ON p.id = t.product_id 
+    LEFT JOIN categories cate ON p.categories_id = cate.id
+    WHERE p.id = ?
     GROUP BY p.id, pi.image_url;
     `,
     [productId]
   );
+  console.log(details)
   return details
 }
 
