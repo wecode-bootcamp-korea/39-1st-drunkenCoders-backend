0bd8bef3430a3296a205a8a47b8466ed41e619c8
MatheGoD <mathe1303@gmail.com';:Wq:wq>
2022-11-24 17:20:24 +0900

    메인

--- src/controllers/commentsController.js
+++ src/controllers/commentsController.js
@@ -1,11 +1,7 @@
 const commentsService = require("../services/commentsService");
 
 const getCommentsByProductId = async (req,res) => {
-<<<<<<< HEAD
-    const {productId} = req.params;
-=======
     const {productId} = req.query;
->>>>>>> main
     const comments = await commentsService.getCommentsByProductId(productId);
 
     return res.status(200).json({data:comments})
